6385440a12714bed1055e22707fc661e2e0f1467
Wanmy <vulong@Wanmy.local>
2024-10-21 23:50:58 +0700

    login

--- ToDoApp/Views/LoginView.swift
+++ ToDoApp/Views/LoginView.swift
@@ -8,12 +8,43 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    @State var isLogin = false
+    
     var body: some View {
         VStack{
             HeaderView()
             //Login form
             
-            //button
+            Form{
+                TextField("Email address", text:$email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Button{
+                    
+                } label: {
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.blue)
+                        Text("Login")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                }
+
+            }
+            //create account
+            VStack{
+                Text("New here?")
+                Button("Create an account") {
+                    
+                }
+            }
+            .padding(.bottom, 10)
             
             Spacer()
         }
