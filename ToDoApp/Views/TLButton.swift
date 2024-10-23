a6d683a5eec8803e8161280270079f77489e88c3
Wanwamy <longvu1217@gmail.com>
2024-10-24 00:12:48 +0700

    button + change view

---
+++ ToDoApp/Views/TLButton.swift
@@ -0,0 +1,18 @@
+//
+//  TLButton.swift
+//  ToDoApp
+//
+//  Created by Wanmy on 23/10/24.
+//
+
+import SwiftUI
+
+struct TLButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TLButton()
+}
